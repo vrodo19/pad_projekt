11df8e3ace85a3240b4b17c87d35f7318e57f609
vrodo19 <66774050+vrodo19@users.noreply.github.com>
2020-06-11 10:59:55 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>    //für serielle Schnittstelle
+#include <termios.h>  //für serielle Schnittstelle
 #include "displayLibrary.h"
 #include "hardwareLibrary.h"
 #include "loggingLibrary.h"
